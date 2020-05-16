43df82480ad7a19dab49668445fad3ea9c2a9894
Alex <shevakov.shura@gmail.com>
2020-05-16 17:19:56 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+const isChecked = true,
+      isClosed = false;
+
+console.log(isChecked && isClosed);
+console.log(isChecked || !isClosed);
